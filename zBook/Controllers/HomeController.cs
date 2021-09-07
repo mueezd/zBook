3461a1aec11328e8bf1429acb0a1c028fd4765e0
deepro <mueezrz@gmail.com>
2021-09-07 23:04:55 +0600

    Add class, in memory data base, also boot strape

--- zBook/Controllers/HomeController.cs
+++ zBook/Controllers/HomeController.cs
@@ -12,9 +12,11 @@ namespace zBook.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, AppDbContext context)
         {
+            _context = context;
             _logger = logger;
         }
 
@@ -33,7 +35,22 @@ namespace zBook.Controllers
         [HttpPost]
         public IActionResult SignUp(SignUpModel signup)
         {
+            CustomerInfo customer = new CustomerInfo();
+            customer.firstName = signup.firstName;
+            customer.lastName = signup.lastName;
+            customer.CompanyName = signup.CompanyName;
+            customer.emailId = signup.emailId;
+            customer.phoneNo = signup.phoneNo;
+            _context.Custmers.Add(customer);
+            _context.SaveChanges();
+
             return View();
+
+        }
+        public IActionResult GetAllCustomer()
+        {
+            var customerlist = _context.Custmers.ToList();
+            return View(customerlist);
         }
         [HttpPost]
         public IActionResult AddCustomer(AddCustomer AddCust)
---
+++ zBook/Models/CustomerInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace zBook.Models
+{
+    public class CustomerInfo
+    {
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string CompanyName { get; set; }
+        public string emailId { get; set; }
+        public string phoneNo { get; set; }
+    }
+}
